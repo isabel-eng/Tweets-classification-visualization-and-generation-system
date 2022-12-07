830d98c4bc97c9f6e1df02a099b45af2abdf128f
victoruribehdz <rodrigouribe05@gmail.com>
2022-12-06 22:11:46 -0600

    model

--- model/LSTM.py
+++ model/LSTM.py
@@ -30,14 +30,11 @@ class LSTM_Classifier:
         else:
             with open('corpus.json', 'r') as corpus_file:
                 self.corpus = json.load(corpus_file)
-            # self.model = keras.models.load_model('model.h5')
             self.tokenizer = Tokenizer(num_words=self.max_words)
             json_file = open('model.json', 'r')
             loaded_model_json = json_file.read()
             json_file.close()
-            # and create a model from that
             self.model = model_from_json(loaded_model_json)
-            # and weight your nodes with your saved values
             self.model.load_weights('model.h5')
 
     def convert_text_to_index_array(self, text):
@@ -52,43 +49,30 @@ class LSTM_Classifier:
     
     def __process_data__(self, ):
         data = self.data
-        # train data
         self.train_x = [x[0] for x in data]
         self.train_y = np.asarray([x[1] for x in data])
-        # print(self.train_x, self.train_y)
 
 
     def __process__corpus__(self, ):
-        # fitting
         self.tokenizer = Tokenizer(num_words=self.max_words)
         self.tokenizer.fit_on_texts(self.train_x)
 
-        # Tokenizers come with a convenient list of words and IDs
         dictionary = self.tokenizer.word_index
-        # Let's save this out so we can use it later
         with open('corpus.json', 'w') as dictionary_file:
             json.dump(dictionary, dictionary_file)
 
 
         def convert_text_to_index_array(text):
-            # one really important thing that `text_to_word_sequence` does
-            # is make all texts the same length -- in this case, the length
-            # of the longest text in the set.
             return [dictionary[word] for word in kpt.text_to_word_sequence(text)]
 
         allWordIndices = []
-        # for each tweet, change each token to its ID in the Tokenizer's word_index
         for text in self.train_x:
             wordIndices = convert_text_to_index_array(text)
             allWordIndices.append(wordIndices)
 
-        # now we have a list of all tweets converted to index arrays.
-        # cast as an array for future usage.
         allWordIndices = np.asarray(allWordIndices)
 
-        # create one-hot matrices out of the indexed tweets
         self.train_x = self.tokenizer.sequences_to_matrix(allWordIndices, mode='binary')
-        # treat the labels as categories
         self.train_y = keras.utils.to_categorical(self.train_y, 2)
 
     def __create_model__(self, ):
@@ -111,14 +95,11 @@ class LSTM_Classifier:
     
     def predict(self, text):
 
-        # format your input for the neural net
         testArr = self.convert_text_to_index_array(text)
         input = self.tokenizer.sequences_to_matrix([testArr], mode='binary')
-        # predict which bucket your input belongs in
         pred = self.model.predict(input)
         print(text)
         self.labels = self.labels[::-1]
-        # and print it for the humons, pred[0][np.argmax(pred)] * 100
         prediction = self.labels[np.argmax(pred)]
         print("the show is: %s" % (prediction))
         return prediction
